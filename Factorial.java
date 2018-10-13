742f106ca7fae5198e135a13471abd707f74c3a4
Akash Dixit <anshudixit00011@gmail.com>
2018-10-13 16:49:02 +0530

    Hacktoberfest2019

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    
+    public static void main(String arg[]){
+        
+        int n=5;
+        int fact =1;
+        int i ; 
+        
+    for(i=1;i<=n;i++){
+        
+         fact = fact*i;
+    }
+       System.out.println("Fact is: " +fact);
+      
+    }
+     
+}
